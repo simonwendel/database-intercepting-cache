51e18b4f3a002a8f2b0ef77f50b02062972af9bd
Simon Wendel <mail@simonwendel.se>
2016-01-18 17:19:18 +0100

    Put interceptors in pass-through mode

---
+++ Cache.Tests/ReaderCachingInterceptorTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Cache.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ReaderCachingInterceptorTests
+    {
+        [Test]
+        public void OnBeforeExecute_WhenInvoked_ShouldDoNothing()
+        {
+            // arrange
+            var sut = new ReaderCachingInterceptor();
+
+            // act
+            sut.OnBeforeExecute(null);
+        }
+
+        [Test]
+        public void OnAfterExecute_WhenInvoked_ShouldDoNothing()
+        {
+            // arrange
+            var sut = new ReaderCachingInterceptor();
+
+            // act
+            sut.OnAfterExecute(null);
+        }
+
+        [Test] // might be integration testing CodeCop, whatever...
+        public void OnOverride_WhenInvokedOnNull_ShouldThrowException()
+        {
+            // arrange
+            var sut = new ReaderCachingInterceptor();
+
+            // assert
+            Assert.Throws<ArgumentNullException>(() => sut.OnOverride(null));
+        }
+    }
+}
--- Cache/ReaderCachingInterceptor.cs
+++ Cache/ReaderCachingInterceptor.cs
@@ -1,24 +1,29 @@
 ﻿namespace Cache
 {
     using System;
+    using System.Linq;
     using CodeCop.Core;
     using CodeCop.Core.Contracts;
+    using CodeCop.Core.Extensions;
 
     internal class ReaderCachingInterceptor : ICopIntercept, ICopOverride
     {
         public void OnAfterExecute(InterceptionContext context)
         {
-            throw new NotImplementedException();
         }
 
         public void OnBeforeExecute(InterceptionContext context)
         {
-            throw new NotImplementedException();
         }
 
         public object OnOverride(InterceptionContext context)
         {
-            throw new NotImplementedException();
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            return context.InterceptedMethod.Execute(context.Sender, context.Parameters.Select(x => x.Value).ToArray());
         }
     }
 }
