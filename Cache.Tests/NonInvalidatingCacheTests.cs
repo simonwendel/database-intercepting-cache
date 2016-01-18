b53ef4ce831563d6e693ea8501de16572df13689
Simon Wendel <mail@simonwendel.se>
2016-01-18 22:56:24 +0100

    Add the IKeyValueStore used by the cache

---
+++ Cache.Tests/NonInvalidatingCacheTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Cache.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture, Category("NonInvalidatingCache")]
+    public class NonInvalidatingCacheTests
+    {
+        [Test]
+        public void Ctor_GivenNullStorage_ThrowsException()
+        {
+            // assert
+            Assert.Throws<ArgumentNullException>(() => new NonInvalidatingCache(null));
+        }
+    }
+}
--- Cache/NonInvalidatingCache.cs
+++ Cache/NonInvalidatingCache.cs
@@ -5,6 +5,18 @@
 
     internal class NonInvalidatingCache : ICache
     {
+        private IKeyValueStore storage;
+
+        public NonInvalidatingCache(IKeyValueStore storage)
+        {
+            if (storage == null)
+            {
+                throw new ArgumentNullException(nameof(storage));
+            }
+
+            this.storage = storage;
+        }
+
         public object CacheSqlDataReader(SqlCommand command, Func<object> fallback)
         {
             throw new NotImplementedException();
