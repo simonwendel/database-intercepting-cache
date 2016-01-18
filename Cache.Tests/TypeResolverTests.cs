e97975676745f38a3ddd932f591923d8ab04f35e
Simon Wendel <mail@simonwendel.se>
2016-01-18 13:06:33 +0100

    Add IoC container and resolver class

---
+++ Cache.Tests/TypeResolverTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Cache.Tests
+{
+    using System.Linq;
+    using CodeCop.Core.Contracts;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TypeResolverTests
+    {
+        [Test]
+        public void ResolveAllOfT_GivenICopOverride_ReturnsRegisteredOverrideAspects()
+        {
+            // act
+            var registeredAspects = TypeResolver.ResolveAll<ICopOverride>();
+
+            // assert
+            Assert.That(registeredAspects.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ResolveAllOfT_GivenICopIntercept_ReturnsRegisteredInterceptorAspects()
+        {
+            // act
+            var registeredAspects = TypeResolver.ResolveAll<ICopIntercept>();
+
+            // assert
+            Assert.That(registeredAspects.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Instance_WhenInvoked_ShouldReturnSingleton()
+        {
+            // act
+            var firstInstance = TypeResolver.Instance;
+            var secondInstance = TypeResolver.Instance;
+
+            // assert
+            Assert.That(firstInstance, Is.SameAs(secondInstance));
+        }
+    }
+}
---
+++ Cache/TypeResolver.cs
@@ -0,0 +1,30 @@
+﻿namespace Cache
+{
+    using System.Collections.Generic;
+    using Ninject;
+
+    internal class TypeResolver
+    {
+        private static TypeResolver instance;
+
+        private IKernel kernel;
+
+        public TypeResolver()
+        {
+            kernel = new StandardKernel();
+        }
+
+        public static TypeResolver Instance
+        {
+            get
+            {
+                return instance ?? (instance = new TypeResolver());
+            }
+        }
+
+        public static IEnumerable<T> ResolveAll<T>()
+        {
+            return Instance.kernel.GetAll<T>();
+        }
+    }
+}
