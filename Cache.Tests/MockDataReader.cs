6b9f6f9a79e59d87d05b776bf5fde0a163bec8fc
Simon Wendel <mail@simonwendel.se>
2016-01-19 18:44:23 +0100

    Add a mock implementation of DbDataReader to tests project

---
+++ Cache.Tests/MockDataReader.cs
@@ -0,0 +1,191 @@
+﻿namespace Cache.Tests
+{
+    using System;
+    using System.Collections;
+    using System.Data;
+    using System.Data.Common;
+
+    internal class MockDataReader : DbDataReader
+    {
+        public override int Depth
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int FieldCount
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public override bool HasRows
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool IsClosed
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override int RecordsAffected
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override object this[string name]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override object this[int ordinal]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool GetBoolean(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte GetByte(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long GetBytes(int ordinal, long dataOffset, byte[] buffer, int bufferOffset, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override char GetChar(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long GetChars(int ordinal, long dataOffset, char[] buffer, int bufferOffset, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetDataTypeName(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DateTime GetDateTime(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override decimal GetDecimal(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetDouble(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerator GetEnumerator()
+        {
+            return new DbEnumerator(this);
+        }
+
+        public override Type GetFieldType(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override float GetFloat(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Guid GetGuid(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override short GetInt16(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetInt32(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long GetInt64(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetName(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetOrdinal(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataTable GetSchemaTable()
+        {
+            return new DataTable();
+        }
+
+        public override string GetString(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object GetValue(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetValues(object[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsDBNull(int ordinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool NextResult()
+        {
+            return false;
+        }
+
+        public override bool Read()
+        {
+            return false;
+        }
+    }
+}
